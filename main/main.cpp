04268ae5b0a1ee0949f49966513f28814a8b1696
EmmanueleVilla <emmanuele.villa@finconsgroup.com>
2022-03-05 13:05:06 +0100

    first commit

---
+++ main/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+// THIS IS OPTIONAL AND NOT REQUIRED, ONLY USE THIS IF YOU DON'T WANT GLAD TO INCLUDE windows.h
+// GLAD will include windows.h for APIENTRY if it was not previously defined.
+// Make sure you have the correct definition for APIENTRY for platforms which define _WIN32 but don't use __stdcall
+#ifdef _WIN32
+    #define APIENTRY __stdcall
+#endif
+
+// GLEW
+//#define GLEW_STATIC
+//#include <glew/glew.h>
+
+#include <glad/glad.h>
+
+// GLFW
+#include <glfw/glfw3.h>
+
+
+// confirm that GLAD didn't include windows.h
+#ifdef _WINDOWS_
+    #error windows.h was included!
+#endif
+
+
+#include<utils/shader_v1.h>
+#include<utils/model_v1.h>
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+// The MAIN function, from here we start the application and run the game loop
+int main()
+{
+    std::cout << "Starting GLFW context" << std::endl;
+    // Init GLFW
+    glfwInit();
+    // Set all the required options for GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Work01", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    // Set the required callback functions
+    glfwSetKeyCallback(window, key_callback);
+
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    //glewExperimental = GL_TRUE;
+    // Initialize GLEW to setup the OpenGL Function pointers
+    //glewInit();
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+
+    // Define the viewport dimensions
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    glEnable(GL_DEPTH_TEST);
+
+    glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+
+    Shader shader("base.vert", "base.frag");
+
+    Model cubeModel("../models/cube.obj");
+
+    shader.Use();
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+
+        // Render
+        // Clear the colorbuffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        cubeModel.Draw();
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
+    shader.Delete();
+    return 0;
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
