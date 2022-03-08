a9e98334ee67ef39233ed33b90e57f9cc08fbd0d
EmmanueleVilla <emmanuele.villa@finconsgroup.com>
2022-03-08 13:40:37 +0100

    rearrange

--- main/main.cpp
+++ main/main.cpp
@@ -94,40 +94,7 @@ GLfloat lastFrame = 0.0f;
 
 /*** TEXTURES ***/
 vector<GLint> textureId;
-
-GLint LoadTexture(const char* path)
-{
-    GLuint textureImage;
-    int w, h, channels;
-    unsigned char* image;
-    image = stbi_load(path, &w, &h, &channels, STBI_rgb);
-
-    if (image == nullptr)
-        std::cout << "Failed to load texture!" << std::endl;
-
-    glGenTextures(1, &textureImage);
-    glBindTexture(GL_TEXTURE_2D, textureImage);
-    // 3 channels = RGB ; 4 channel = RGBA
-    if (channels==3)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    else if (channels==4)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    // we set how to consider UVs outside [0,1] range
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // we set the filtering for minification and magnification
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-
-    // we free the memory once we have created an OpenGL texture
-    stbi_image_free(image);
-
-    // we set the binding to 0 once we have finished
-    glBindTexture(GL_TEXTURE_2D, 0);
-
-    return textureImage;
-}
+GLint LoadTexture(const char* path);
 
 /////////////////// MAIN function ///////////////////////
 int main()
@@ -317,4 +284,38 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
   if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
       glfwSetWindowShouldClose(window, GL_TRUE);
   }
+}
+
+GLint LoadTexture(const char* path)
+{
+    GLuint textureImage;
+    int w, h, channels;
+    unsigned char* image;
+    image = stbi_load(path, &w, &h, &channels, STBI_rgb);
+
+    if (image == nullptr)
+        std::cout << "Failed to load texture!" << std::endl;
+
+    glGenTextures(1, &textureImage);
+    glBindTexture(GL_TEXTURE_2D, textureImage);
+    // 3 channels = RGB ; 4 channel = RGBA
+    if (channels==3)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    else if (channels==4)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    // we set how to consider UVs outside [0,1] range
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // we set the filtering for minification and magnification
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+
+    // we free the memory once we have created an OpenGL texture
+    stbi_image_free(image);
+
+    // we set the binding to 0 once we have finished
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    return textureImage;
 }
